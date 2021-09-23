92ac67e77d77f4e621e3eead0265cf9e73ed0fa5
SK-Studios <shenkit108@gmail.com>
2021-09-23 22:13:49 +0800

    Finished UI for TagTile class and Dialog to edit tags, need to make add tag dialog

--- workout_app/lib/routes/edit_tags.dart
+++ workout_app/lib/routes/edit_tags.dart
@@ -22,44 +22,68 @@ class _EditTagsState extends State<EditTags> {
     Color(0xFFCFA9FF),
     Color(0xFFD37F31),
   ];
+  final int colorsInRow = 5;
 
   void onColorChanged(Color color) => setState(() => currentColor = color);
 
-  Future<void> _showEditTagDialog(context) async {
+  Future<void> showEditTagDialog(context) async {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Edit Tag'),
-          content: Column(
-            children: [
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Name',
+        return Dialog(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: Column(
+              children: [
+                const Text(
+                  'Edit Tag',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: 300,
-                child: ListView(
-                  children: [
-                    BlockPicker(
-                      pickerColor: currentColor,
-                      onColorChanged: onColorChanged,
-                      availableColors: availableColors,
+                const SizedBox(height: 20),
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Name',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                BlockPicker(
+                  pickerColor: currentColor,
+                  onColorChanged: onColorChanged,
+                  availableColors: availableColors,
+                  layoutBuilder: (BuildContext context, List<Color> colors,
+                      PickerItem child) {
+                    return SizedBox(
+                      width: 300,
+                      height: colors.length / colorsInRow * 70,
+                      child: GridView.count(
+                        crossAxisCount: colorsInRow,
+                        crossAxisSpacing: 5.0,
+                        mainAxisSpacing: 5.0,
+                        children:
+                            colors.map((Color color) => child(color)).toList(),
+                      ),
+                    );
+                  },
+                ),
+                TextButton(
+                  child: const Text(
+                    'Save',
+                    style: TextStyle(
+                      color: Color(0xFF90CAF9),
                     ),
-                  ],
-                ),
-              ),
-            ],
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
           ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Save'),
-              onPressed: () => Navigator.of(context).pop(),
-            )
-          ],
         );
       },
     );
@@ -73,60 +97,24 @@ class _EditTagsState extends State<EditTags> {
       ),
       body: ListView(
         children: <Widget>[
-          Slidable(
-            actionPane: const SlidableScrollActionPane(),
-            actions: <Widget>[
-              IconSlideAction(
-                caption: 'Delete',
-                color: Colors.red,
-                icon: Icons.delete,
-                onTap: () {},
-              )
-            ],
-            actionExtentRatio: 1 / 5,
-            child: SizedBox(
-              height: 60,
-              width: double.maxFinite,
-              child: Stack(
-                alignment: Alignment.center,
-                fit: StackFit.expand,
-                children: <Widget>[
-                  TextButton(
-                    onPressed: () => _showEditTagDialog(context),
-                    child: const Text(
-                      'tag name',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  // tag colour circle
-                  Positioned(
-                    left: 10,
-                    child: Container(
-                      height: 20,
-                      width: 20,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlue,
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                    ),
-                  ),
-                  //divider
-                  const Positioned(
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    child: Divider(height: 1, thickness: 1),
-                  ),
-                ],
-              ),
-            ),
+          TagTile(
+            id: 0,
+            name: 'Push',
+            color: const Color(0xFF9BA1FF),
+            showEditTagDialog: showEditTagDialog,
           ),
-          // TagTile(id: 0, name: 'Push', color: Color(0xFF9BA1FF)),
-          // TagTile(id: 1, name: 'Pull', color: Color(0xFFFFF3AB)),
-          // TagTile(id: 2, name: 'Horizontal', color: Color(0xFFFF9FCE)),
+          TagTile(
+            id: 1,
+            name: 'Pull',
+            color: const Color(0xFFFFF3AB),
+            showEditTagDialog: showEditTagDialog,
+          ),
+          TagTile(
+            id: 2,
+            name: 'Horizontal',
+            color: const Color(0xFFFF9FCE),
+            showEditTagDialog: showEditTagDialog,
+          ),
         ],
       ),
     );
@@ -135,15 +123,17 @@ class _EditTagsState extends State<EditTags> {
 
 class TagTile extends StatelessWidget {
   const TagTile({
-    Key? key,
     required this.id,
     required this.name,
     required this.color,
+    required this.showEditTagDialog,
+    Key? key,
   }) : super(key: key);
 
   final int id;
   final String name;
   final Color color;
+  final Function showEditTagDialog;
 
   @override
   Widget build(BuildContext context) {
@@ -166,7 +156,9 @@ class TagTile extends StatelessWidget {
           fit: StackFit.expand,
           children: <Widget>[
             TextButton(
-              onPressed: () => {}, //_showEditTagDialog(context),
+              onPressed: () {
+                showEditTagDialog(context);
+              },
               child: Text(
                 name,
                 style: const TextStyle(
